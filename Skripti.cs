d3c9f42e04b38b49d792d235bc8a4428b5e4c1fe
pessiraunio <60428514+pessiraunio@users.noreply.github.com>
2020-02-05 13:05:11 +0200

    Add files via upload
    tääseo

---
+++ Skripti.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while (n==1)
+
+            Console.WriteLine("Osaamisesi asteikolla 1-10?");
+            int x =int.Parse(Console.ReadLine());
+
+
+            if (x > 0 && x < 4)
+            {
+                Console.WriteLine("Olet aika nuubi");
+            }
+
+            else if (x > 4 && x<7 || x==4)
+            {
+                Console.WriteLine("Olet perse");
+            }
+
+            else
+            {
+                Console.WriteLine("OOt iha himo hyvä!!!");
+            }
+ 
+        }
+    }
+}
